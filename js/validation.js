3e6a4ebe466f8a415f3f67bd5e8de4504ddcae17
Andres Morales <andr.moralesc@duocuc.cl>
2023-05-12 23:14:32 -0400

    validacion login

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,18 @@
 $(document).ready(function(onclick){
-    $("#ingresar").submit(function(event){
+    $("#formulario").submit(function(event){
 
     let user = $("#usuario").val();
     let pass =$("#password").val();
 
-    if(user==""|| pass=="");
+    if(user==""|| pass==""){
     alert("Debe completar este campo") ;
-    return false;
+    return false
+    }
+    else{
+        window.location.href="index.html"
+    }
 });
+
 });
 
 
