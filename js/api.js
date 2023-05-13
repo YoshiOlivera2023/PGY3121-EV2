cb48a92faa29ea2319b3eeda266710ef18284dc2
Juan Carlos Toledo <jtoledo.externo@coopeuch.cl>
2023-05-12 22:51:20 -0400

    Mejoras Front-End y API

---
+++ js/api.js
@@ -0,0 +1,14 @@
+$(document).ready(function () { 
+      $.ajax({  
+        type: "GET",  
+        url: "https://mindicador.cl/api",    
+        dataType: "json", 
+        success: function (data) {              
+            let fecha = data.fecha;             
+            let indicadores="<p>Indicadores Economicos >>> Fecha: "+fecha.substring(0,10)+" - UF: "+data.uf.valor+" - DOLAR: "+data.dolar.valor+" - EURO: "+data.euro.valor+" - IPC:"+data.ipc.valor+"% - UTM:"+data.utm.valor+"</p>";
+            $("#textoIndicadores").append(indicadores);     
+        }, 
+    });  
+  
+   
+  });
